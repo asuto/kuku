f1b85d144491ec4f45b8601d531f45a1501b7006
MAEDA <asuranji1993@yahoo.co.jp>
2014-05-17 13:40:23 +0900

    kadai

---
+++ Source.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	printf("<!DOCTYPE html1>\n");
+	printf("<mata charset=\"UTF-8\">\n");
+	printf("<title>99</title>\n");
+	printf("<hi>99</hi>\n");
+	printf("<table>\n");
+
+	for(x=0 ; x<10 ; x++)
+	{
+		printf("<tr>\n");
+		printf("<th>%2d",x);
+		for(y=1;y<10;y++)
+		{
+			if(x==0)
+			{
+				printf("<th>%2d",y);
+			}
+			else
+			{
+				printf("<th>%2d",x*y);
+			}
+	
+		}
+	}
+	return 0;
+}
